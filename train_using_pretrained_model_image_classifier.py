d20404deee543688132e4f13c9de0e5b08106485
Eyüp Ensar Kara <eyupensarkara04@gmail.com>
2025-05-08 15:14:52 +0300

    Pretrained ViT dönüşümleri güncellendi; eğitim dönüşümleri, doğrudan pretrained_vit_transforms ile değiştirildi.

--- train_using_pretrained_model_image_classifier.py
+++ train_using_pretrained_model_image_classifier.py
@@ -61,7 +61,7 @@ train_transforms = transforms.Compose([
     transforms.RandomRotation(degrees=15),
     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
     transforms.RandomResizedCrop(size=224, scale=(0.8, 1.0)),
-    *pretrained_vit_transforms.transforms
+    pretrained_vit_transforms
 ])
 
 test_transforms = pretrained_vit_transforms
